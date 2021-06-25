96a19e00cb7112877d246abc9e18ac0020b7c26a
mattrossman <rossman@gatech.edu>
2021-06-25 16:37:47 -0400

    Add twind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import twindJsx from '@twind/vite-plugin-jsx'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh(), twindJsx()],
+  esbuild: {
+    jsxInject: `import React from "react"`,
+  },
 })
