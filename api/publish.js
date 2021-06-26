6123d4ff35f148fc5062ea8d40dc9de07f6b9a9d
mattrossman <rossman@gatech.edu>
2021-06-25 23:13:06 -0400

    Obfuscate IPs and URLs

--- api/publish.js
+++ api/publish.js
@@ -1,11 +1,19 @@
-const { VercelRequest, VercelResponse } = require('@vercel/node')
+/** @typedef {import('@vercel/node').VercelRequest} VercelRequest */
+/** @typedef {import('@vercel/node').VercelResponse} VercelResponse */
+
 const admin = require('./utils/admin')
 
 const db = admin.database()
-const rooms = db.ref('/rooms')
+const rooms = db.ref('rooms')
+
+const crypto = require('crypto')
+const Hashids = require('hashids')
 
 /**
- * Publish a link to your room under /rooms/[ip]
+ * @typedef {object} LinkRequest
+ * @property {string} url - The URL to publish
+ *
+ * POST /api/publish: Publish a link to your room under /rooms/[ip]
  * @param {VercelRequest} req
  * @param {VercelResponse} res
  */
@@ -14,15 +22,15 @@ module.exports = async (req, res) => {
   const body = req.body
   const ip = req.headers['x-real-ip']
 
-  try {
-    await rooms.set({ [ip]: body.url })
-    res.send(`Published ${body.url} to room ${ip}`)
-  } catch (e) {
-    res.status(400).send('Error calling /publish endpoint')
-  }
-}
+  // Hash the IP to obfuscate it in the database
+  const ipHashed = crypto.createHash('sha256').update(ip).digest('hex')
 
-/**
- * @typedef {object} LinkRequest
- * @property {string} url - The URL to publish
- */
+  // Use digest to obfuscate the URL in the database
+  const hashids = new Hashids(ipHashed)
+  const encoder = new TextEncoder()
+  const arrayView = encoder.encode(body.url)
+  const urlHashed = hashids.encode(...arrayView)
+
+  await rooms.set({ [ipHashed]: urlHashed })
+  res.send(`Published ${body.url} to room ${ip}`)
+}
